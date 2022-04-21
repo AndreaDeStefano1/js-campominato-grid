03a0a7ad4223efed176975d27226dfa6a7717a35
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-04-21 17:42:41 +0200

    Esercizio finito

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const box = document.querySelector('.box');
+const playButton = document.getElementById('start-game');
 
 function selectDifficulty(){
-  let difficulty = document.getElementById(difficulty).value
-  if(difficulty === easy){
+  let difficulty = document.getElementById('difficulty').value
+  if(difficulty === 'easy'){
     return 49
-  }else if (difficulty === hard) {
+  }else if (difficulty === 'hard') {
     return 81
-  }else if (difficulty === crazy) {
+  }else if (difficulty === 'crazy') {
     return 100 
   }
 }
+
 function squareGenerator(n){
   for (let i = 0; i < n; i++){
-    const square = document.createElement('div')
+    const square = document.createElement('div');
     square.classList.add('square');
-    const number = document.createElement('span')
+    const number = document.createElement('span');
+    number.classList.add('d-none');
+
     // number.classList.add('');
     if(n == 49){
       square.classList.add('square-49');
@@ -26,6 +30,15 @@ function squareGenerator(n){
     box.append(square);
     number.innerText = `${(i + 1)}`;
     square.append(number);
+    square.addEventListener('click', function(){
+      number.classList.remove('d-none');
+      this.classList.add('blue');
+    })
   }
-  
 }
+
+playButton.addEventListener('click', function(){
+  box.innerHTML = '';
+  let diff = selectDifficulty();
+  squareGenerator(diff);
+})
